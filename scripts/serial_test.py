ee52d414590fd9d3e14325f985603f0b8bca6106
Christian Hellum Bye <chbye@berkeley.edu>
2024-02-29 16:46:00 -0800

    modularize serial code

--- scripts/serial_test.py
+++ scripts/serial_test.py
@@ -1,34 +1,40 @@
 import serial
 import time
 
+def bit2volt(analog_value, vmax=3.3, nbits=16):
+    ratio = vmax / (2**nbits - 1)
+    return ratio * analog_value
+
+
 def serial_connect(port, operator, threshold):
-    baudrate = 921600
-    #port for 1st pico is "/dev/ttyACM0"
+    baudrate = 115200 # 921600
     ser = serial.Serial(port, baudrate)
 
     ser.dtr = False
-    time.sleep(1)
+    time.sleep(0.5)
     ser.dtr = True
-
-    analog_value=0
 
     while True:
         if ser.in_waiting:
             analog_str = ser.readline().decode('utf-8').strip()
-            analog_value = int(analog_str)
-            volt_value = (3.3/65535)*analog_value
-            print("Analog Value: ", analog_value, " Voltage Value: ", volt_value)
-            if operator == 'higher':
-                if analog_value >= threshold:
-                    print('Done!')
-                    break
-            elif operator == 'lower':
-                if analog_value <= threshold:
-                    print('Done!')
-                    break
-            else:
-                print('Invalid operator.')
-                break
+            a, b = analog_str.split()
+            a = int(a)
+            b = int(b)
+            analog_values = np.array([a, b])
+            volt_values = bit2volt(analog_values)
+            print("Analog Value: ", analog_values, " Voltage Value: ", volt_values)
+            # XXX
+            # if operator == 'higher':
+            #     if analog_value >= threshold:
+            #         print('Done!')
+            #         break
+            # elif operator == 'lower':
+            #     if analog_value <= threshold:
+            #         print('Done!')
+            #         break
+            # else:
+            #     print('Invalid operator.')
+            #     break
 
 if __name__ == "__main__":
     port = "/dev/ttyACM0"
