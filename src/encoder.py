855c777b37232bd55181300f870599281a0811bc
Christian Hellum Bye <chbye@berkeley.edu>
2024-02-29 16:45:35 -0800

    add pot class, move serial method

--- src/encoder.py
+++ src/encoder.py
@@ -1,47 +1,53 @@
 import serial
 import time
 from qwiic_dual_encoder_reader import QwiicDualEncoderReader
-
+from .motor import MOTOR_ID
 
 class Encoder(QwiicDualEncoderReader):
 
-    def get_encoder(self, motor_id):
-        if motor_id == 0:
+    def get_encoder(self, motor):
+        """
+        Read the encoder count of a motor.
+
+        Parameters
+        ----------
+        motor : str
+            Either ``az'' or ``alt''. The motor to read the encoder value of.
+        """
+        mid = MOTOR_ID[motor]
+        if mid == 0:
             return self.encoder.count1
-        elif motor_id == 1:
+        elif mid == 1:
             return self.encoder.count2
-        else:
-            raise KeyError("Invalid motor id.")
 
-    def serial_read(self):
-        baudrate = 115200
-        # port for 1st pico is "/dev/ttyACM0"
-        port = "/dev/ttyACM0"
-        ser = serial.Serial(port, baudrate)
+class Pot:
 
-        ser.dtr = False
-        time.sleep(0.5)
-        ser.dtr = True
+    VMAX = 3.3  # voltage range
+    NBITS = 16  # ADC number of bits
 
+    def __init__(self, motors=["az", "alt"])
+        """
+        Class for reading voltages from the potentiometers.
+        """
+        #XXX should specify baud rate, port etc and open the serial connection.
+        pass
 
-        while True:
-            if ser.in_waiting:
-                analog_string = ser.readline().decode("utf-8").strip()
-                print(analog_string)
-                a, b = analog_string.split()
-                analog_value_0 = int(a)
-                analog_value_1 = int(b)
-                volt_value_0 = (3.3 / 65535) * analog_value_0
-                volt_value_1 = (3.3 / 65535) * analog_value_1
-                print(
-                    "Analog Value 0: ",
-                    analog_value_0,
-                    " Voltage Value 0: ",
-                    volt_value_0,
-                    "  Analog Value 1: ",
-                    analog_value_1,
-                    " Voltage Value 1: ",
-                    volt_value_1,
-                )
-        # port = "/dev/ttyACM0"
-        # serial_connect(port, 'higher', 50000)
+    def bit2volt(self, analog_value):
+        res = 2 ** self.NBITS - 1
+        ratio = self.VMAX / res
+        return ratio * analog_value
+
+    def read_analog(self, motor):
+        """
+        Read the analog value from a pot.
+        """
+        raise NotImplementedError
+
+    def read_volts(self, motor):
+        analog = self.read_analog()
+        return self.bit2volt(analog)
+
+    def get_position(self, motor):
+        v = self.read_volts()
+        #XXX convert voltage to position
+        raise NotImplementedError
