8f84d8e6ff60b0e04104d59f37f24ac26b608aec
Christian Hellum Bye <chbye@berkeley.edu>
2024-03-04 10:08:14 -0800

    simplify code

--- scripts/main.py
+++ scripts/main.py
@@ -1,28 +1,22 @@
-from machine import ADC, Pin
+from machine import ADC, Pin, UART
 from time import sleep
 
-adc1 = ADC(Pin(27))
-adc2 = ADC(Pin(28))
-uart = machine.UART(0, baudrate = 115200)
-count1 = 0
-count2 = 0
-value1 = 0
-value2 = 0
-placeholder1 = 0
-placeholder2 = 0
+ADC_PIN1 = 27
+ADC_PIN2 = 28
+BAUDRATE = 115200
+INT_LEN = 10  # number of readings to average
+SLEEP = 0.05  # seconds between readings
+
+adc1 = ADC(Pin(ADC_PIN1))
+adc2 = ADC(Pin(ADC_PIN2))
+uart = UART(0, baudrate=BAUDRATE)
 
 while True:
-    value1 += adc1.read_u16()
-    value2 += adc2.read_u16()
-    count1 += 1
-    count2 += 1
-    if count1 >= 10:
-        placeholder1 = (int(value1/count1))
-        value1 = 0
-        count1 = 0
-    if count2 >= 10:
-        placeholder2 = (int(value2/count2))
-        value2 = 0
-        count2 = 0
-    print(placeholder1, placeholder2)
-    sleep(0.05)
+    cnt = 0
+    value1 = 0
+    value2 = 0
+    while cnt < INT_LEN:
+        value1 += adc1.read_u16()
+        value2 += adc2.read_u16()
+        sleep(SLEEP)
+    print(value1 / INT_LEN, value2 / INT_LEN)
