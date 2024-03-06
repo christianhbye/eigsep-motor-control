75675f04b1ebc2ed4ffacf3a2f6f41cefd86d518
Christian Hellum Bye <chbye@berkeley.edu>
2024-03-05 16:20:18 -0800

    initial commit

---
+++ scripts/run.py
@@ -0,0 +1,44 @@
+import serial
+import time
+from threading import Event, Thread
+import eigsep_motor_control as emc
+
+# motor velocities
+AZ_VEL = 254
+ALT_VEL = 254
+# serial connection (pi to pico)
+PORT = "/dev/ttyACM0"
+BAUDRATE = 115200
+
+ser = serial.Serial(PORT, BAUDRATE)
+# ser.dtr = False
+# time.sleep(0.5)
+ser.dtr = True
+# time.sleep(0.5)
+
+pot = emc.Potentiometer(motors=["az", "alt"])
+# create events that tells motors to reverse direction
+az_reverse = Event()
+alt_reverse = Event()
+# thread monitoring potentiometer voltage and setting events
+thd = Thread(target=pot.monitor, args=(az_reverse, alt_reverse), daemon=True)
+thd.start()
+
+# start motors
+motor = emc.Motor()
+motor.start(az_vel=AZ_VEL, alt_vel=ALT_VEL)
+
+while True:
+    try:
+        if az_reverse.is_set():
+            motor.reverse("az")
+            az_reverse.clear()
+        if alt_reverse.is_set():
+            motor.reverse("alt")
+            alt_reverse.clear()
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+        break
+
+motor.stop(motors=["az", "alt"])
+motor.stow(motors=["az", "alt"])
