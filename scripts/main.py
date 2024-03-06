fec2f393eb1429c51b496a014e2a894182836135
Christian Hellum Bye <chbye@berkeley.edu>
2024-03-05 17:43:52 -0800

    explictly pack data with struct

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,5 @@
 from machine import ADC, Pin, UART
+import struct
 from time import sleep
 
 ADC_PIN1 = 27
@@ -9,8 +10,8 @@ SLEEP = 0.05  # seconds between readings
 
 adc1 = ADC(Pin(ADC_PIN1))
 adc2 = ADC(Pin(ADC_PIN2))
-uart = UART(0, baudrate=BAUDRATE)  # XXX is this necessary?
-#uart.init?
+uart = UART(0)
+uart.init(baudrate=BAUDRATE)
 
 while True:
     cnt = 0
@@ -20,4 +21,5 @@ while True:
         value1 += adc1.read_u16()
         value2 += adc2.read_u16()
         sleep(SLEEP)
-    print(value1 / INT_LEN, value2 / INT_LEN)
+    data = struct.pack("<ff", value1 / INT_LEN, value2 / INT_LEN)
+    uart.write(data)
