9b55eaf0f28a62a9450ce403975e268ecf2dc1a1
RPi 4 <chbye@berkeley.edu>
2024-03-11 19:25:12 +0000

    add print statements

--- scripts/run.py
+++ scripts/run.py
@@ -4,31 +4,37 @@ import eigsep_motor_control as emc
 
 # motor velocities
 AZ_VEL = 254
-ALT_VEL = 254
+ALT_VEL = 0#254
 
-pot = emc.Potentiometer(motors=["az", "alt"])
+pot = emc.Potentiometer()
 # create events that tells motors to reverse direction
 az_reverse = Event()
 alt_reverse = Event()
 # thread monitoring potentiometer voltage and setting events
 thd = Thread(target=pot.monitor, args=(az_reverse, alt_reverse), daemon=True)
+print("starting pot thread")
 thd.start()
 
 # start motors
+print("start motors")
 motor = emc.Motor()
 motor.start(az_vel=AZ_VEL, alt_vel=ALT_VEL)
 
 while True:
+    print("...")
     try:
         if az_reverse.is_set():
+            print("reverse az")
             motor.reverse("az")
             az_reverse.clear()
         if alt_reverse.is_set():
+            print("reverse alt")
             motor.reverse("alt")
             alt_reverse.clear()
         time.sleep(0.1)
+        continue
     except KeyboardInterrupt:
         break
 
 motor.stop(motors=["az", "alt"])
-motor.stow(motors=["az", "alt"])
+#motor.stow(motors=["az", "alt"])
