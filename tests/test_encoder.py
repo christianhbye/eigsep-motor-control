70ffbfa59fc919af23c84744d8dd03ff3fb20786
Christian Hellum Bye <chbye@berkeley.edu>
2024-03-12 11:59:26 -0700

    blacken

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -4,12 +4,14 @@ import pytest
 
 import eigsep_motor_control as emc
 
+
 def _generate_data(seed):
     """Generate analog data from a Pico"""
     rng = np.random.default_rng(seed=seed)
     size = emc.serial_params.INT_LEN
     val = rng.integers(0, 2**16, size=size, dtype=np.uint16, endpoint=False)
     return np.sum(val)
+
 
 @pytest.mark.parametrize("seed", [0, 42, 2024, 7385])
 def test_read_analog(seed):
