cfe5e71bbe8b87b00b099927c749e06361ce0d8c
RPi 4 <chbye@berkeley.edu>
2024-03-14 18:29:26 +0000

    formatting

--- scripts/read_pot.py
+++ scripts/read_pot.py
@@ -2,9 +2,8 @@ import time
 from eigsep_motor_control import Potentiometer
 
 pot = Potentiometer()
-print(pot.TIMEOUT)
 
 while True:
     v = pot.read_volts()
-    print(v)
+    print(f"az: {v[0]:.3f}, alt: {v[1]:.3f}")
     time.sleep(0.1)
