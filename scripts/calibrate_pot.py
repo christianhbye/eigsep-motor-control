40f452812b61e8f6d0cc5a2c19914d8ef771488d
Christian Hellum Bye <chbye@berkeley.edu>
2024-04-03 12:15:01 -0700

    put main loop in function, call new pot methods

--- scripts/calibrate_pot.py
+++ scripts/calibrate_pot.py
@@ -4,90 +4,103 @@ import time
 import numpy as np
 import eigsep_motor_control as emc
 
-parser = ArgumentParser(description="Calibrate potentiometers.")
-parser.add_argument(
-    "-a", "--az", action="store_true", help="Calibrate azimuth pot."
-)
-parser.add_argument(
-    "-e", "--el", action="store_true", help="Calibrate elevation pot."
-)
-args = parser.parse_args()
 
-motors = []
-if args.az:
-    motors.append("az")
-    motors.append("az_reverse")
-if args.el:
-    motors.append("alt")
-    motors.append("alt_reverse")
-if not motors:
-    print("Exiting...")
-    sys.exit()
+def calibrate(motor, direction):
+    """
+    Calibrate the potentiometer corresponding to the motor.
 
-motor = emc.Motor()
-pot = emc.Potentiometer()
-vmax = {}
+    Parameters
+    ----------
+    motor : str
+        Motor to calibrate. Either 'az' or 'alt'.
+    direction : int
+        Direction of the motor. 1 for forward (increasing pot voltages),
+        -1 for reverse (decreasing pot voltages).
 
-for m in motors:
-    print(f"Calibrating {m.upper()} motor")
-    if m == "az":
+    Returns
+    -------
+    vm : float
+        Maximum/minimum pot voltage (depending on the direction).
+    stuck : bool
+        True if the pot was stuck, False otherwise.
+
+    """
+    if motor == "az":
+        motor_id = 0
         az_vel = 254
         alt_vel = 0
-    elif m == "alt":
+    elif motor == "alt":
+        motor_id = 1
         az_vel = 0
         alt_vel = 254
-    elif m == "az_reverse":
-        az_vel = -254
-        alt_vel = 0
-    elif m == "alt_reverse":
-        az_vel = 0
-        alt_vel = -254
+    else:
+        raise ValueError("Invalid motor.")
 
+    az_vel *= direction
+    alt_vel *= direction
+
+    motor = emc.Motor()
+    pot = emc.Potentiometer()
     motor.start(az_vel=az_vel, alt_vel=alt_vel)
-    try:
-        volts = []
-        while len(volts) < 2:
-            volts.append(pot.read_volts(motor=m))
-            time.sleep(0.1)
-        # loops until the switch is triggered
-        print("Attack mode.")
-        while volts[-1] >= volts[-2]:  # XXX check the tolerance
-            v = pot.read_volts(motor=m)
-            print(v)
-            volts.append(v)
-            time.sleep(0.1)
-        vmax[m] = np.max(volts)
-        # now: either the pot is stuck or the switch is triggered
-        while volts[-1] > vmax[m] - 0.1:  # XXX (somewhat) arbitrary threshold
-            v = pot.read_volts(motor=m)
-            print(v)
-            volts.append(v)
-            time.sleep(0.1)
-        print("Triggered the switch.")
-        # now we're certain that we're reversing
-        # check if the pot was stuck
-        if np.count_nonzero(np.abs(v - vmax[m]) < 0.01) > 2:  # XXX
-            print("Got the pot stuck.")
-            stuck = True
-        else:
-            print("Did not get the pot stuck.")
-            stuck = False
-        print("Reverse until switch is released!")
-        while volts[-1] <= volts[-2]:
-            v = pot.read_volts(motor=m)
-            print(v)
-            volts.append(v)
-            time.sleep(0.1)
-        if stuck:
-            print("Calibration done.")
-            motors.remove(f"{m}_reverse")  # don't need to do the reverse
-        else:  # didn't get the pot stuck, run until the other end
-            motor.stop()
+    pot.reset_volt_readings()
 
-    except KeyboardInterrupt:
-        print("Interrupting.")
-    finally:
-        print("Stopping.")
-        motor.stop()
+    # loops until the switch is triggered
+    print("Attack mode.")
+    while pot.direction[motor] == direction:
+        v = pot.read_volts(motor=motor)
+        print(v)
+        time.sleep(0.1)
+    # get the extremum pot voltage (max if forward, min if reverse)
+    vm = np.max(direction * pot.volts[:, motor_id])
 
-print(vmax)
+    # now: either the pot is stuck or the switch is triggered
+    # this loops runs as long as the pot is stuck
+    tol = 0.01  # XXX arbitrary threshold for pot being stuck
+    stuck_cnt = np.count_nonzero(np.abs(pot.vdiff[motor]) < tol)
+    while np.abs(pot.vdiff[motor])[-1] < tol:
+        v = pot.read_volts(motor=motor)
+        print(v)
+        stuck_cnt += 1
+        time.sleep(0.1)
+    print("Reverse until switch is released.")
+    while pot.direction[motor] == -direction:
+        v = pot.read_volts(motor=motor)
+        print(v)
+        time.sleep(0.1)
+
+    return vm, stuck_cnt > 2
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(description="Calibrate potentiometers.")
+    parser.add_argument(
+        "-a", "--az", action="store_true", help="Calibrate azimuth pot."
+    )
+    parser.add_argument(
+        "-e", "--el", action="store_true", help="Calibrate elevation pot."
+    )
+    args = parser.parse_args()
+
+# #XXX
+# motors = []
+# if args.az:
+#     motors.append("az")
+#     motors.append("az_reverse")
+# if args.el:
+#     motors.append("alt")
+#     motors.append("alt_reverse")
+# if not motors:
+#     print("Exiting...")
+#     sys.exit()
+#
+#     #XXX
+#     try:
+#         pass
+#     except KeyboardInterrupt:
+#         print("Interrupting.")
+#     finally:
+#         print("Stopping.")
+#         motor.stop()
+#
+# print(vmax)
