c2df87e67e7a86406e8919885a536b4d6f8d0652
Christian Hellum Bye <chbye@berkeley.edu>
2024-04-18 13:23:27 -0700

    clean up and simplify code

--- eigsep_motor_control/limit_switch_hit.py
+++ eigsep_motor_control/limit_switch_hit.py
@@ -1,49 +1,38 @@
-from argparse import ArgumentParser
-import sys
 import time
-import numpy as np
-import eigsep_motor_control as emc
+
 
 def limit_switch(motor, m, pot):
-    
+
     az_vel = 0
-    alt_vel = 0    
-    
+    alt_vel = 0
+
     if motor == "az":
         az_dir, az_vel = m.velocities["az"]
     elif motor == "alt":
         alt_dir, alt_vel = m.velocities["alt"]
     else:
         raise ValueError("Invalid motor.")
-    
+
     if az_vel == 0 & alt_vel == 0:
         return False
-    
-    #pot.reset_volt_readings()
-    #time.sleep(3)
-    #print(pot.direction[motor], az_dir)   
+
+    # pot.reset_volt_readings()
+    # time.sleep(3)
+    # print(pot.direction[motor], az_dir)
     if pot.direction[motor] == az_dir:
         return False
     if pot.direction[motor] != az_dir:
         return True
 
+
 def reverse_limit(m, pot, limits):
-    new_limits = [limits[0], limits[1]]
-    #print(limit_switch("az", m, pot))
-    if limit_switch("az", m, pot) and not new_limits[0].is_set():
-        print("Setting event")
-        new_limits[0].set()
-    elif not limit_switch("az", m, pot) and new_limits[0].is_set():
-        m.reverse("az")
-        time.sleep(10)
-        new_limits[0].clear()
-    print(new_limits[0].is_set())    
-
-    if limit_switch("alt", m, pot) and not new_limit[1].is_set():
-        new_limits[1].set()
-    elif not limit_switch("alt", m, pot) and new_limits[1].is_set():
-        m.reverse("alt")
-        time.sleep(1)
-        new_limits[1].clear()
-
-    return new_limits
+    # print(limit_switch("az", m, pot))
+    for motor, limit in zip(["az", "alt"], limits):
+        if limit_switch(motor, m, pot) and not limit.is_set():
+            print(f"Setting event for {motor}")
+            limit.set()
+        elif not limit_switch(motor, m, pot) and limit.is_set():
+            m.reverse(motor)
+            time.sleep(10)  # XXX
+            limit.clear()
+    return limits
