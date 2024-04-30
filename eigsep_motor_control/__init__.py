f29b8554864cadcfeae25e08c4ae94f43cecbce5
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-04-30 16:38:57 -0700

    imports new classes QwiicMotor and PoluluMotor

--- eigsep_motor_control/__init__.py
+++ eigsep_motor_control/__init__.py
@@ -2,5 +2,6 @@ __author__ = "EIGSEP Team"
 __version__ = "0.0.1"
 
 from .encoder import Encoder, Potentiometer
-from .motor import Motor
+from .motor import Motor, QwiicMotor, PoluluMotor
 from .limit_switch_hit import reverse_limit
+
