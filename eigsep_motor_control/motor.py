f114a97b130f66ea5aabf9e9bc5e92e96fc1bd88
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-04-30 17:56:21 -0700

    removed PoluloMotor class from try/catch

--- eigsep_motor_control/motor.py
+++ eigsep_motor_control/motor.py
@@ -1,6 +1,10 @@
 import numpy as np
 import time
 from qwiic_scmd import QwiicScmd
+try:
+    from dual_max14870_rpi import motors, MAX_SPEED
+except ImportError:
+    pass
 from abc import ABC, abstractmethod
 
 MOTOR_ID = {"az": 0, "alt": 1}
@@ -80,46 +84,41 @@ class QwiicMotor(Motor, QwiicScmd):
         """Stow method for returning motors to a home position."""
         raise NotImplementedError("Stow method not implemented.")
 
-try:
-    from dual_max14870_rpi import motors, MAX_SPEED
+class PoluluMotor(Motor):
+    def __init__(self):
+        super().__init__()
 
-    class PoluluMotor(Motor):
-        def __init__(self):
-            super().__init__()
+    def start(self, az_vel=MAX_SPEED, alt_vel=MAX_SPEED):
+        """Starts both motors (azimuth and altitude) with the given velocities."""
+        az_direction = 1 if az_vel > 0 else 0
+        alt_direction = 1 if alt_vel > 0 else 0
+        motors.setSpeeds(az_vel, alt_vel)
+        self.velocities["az"] = (az_direction, abs(az_vel))
+        self.velocities["alt"] = (alt_direction, abs(alt_vel))
 
-        def start(self, az_vel=MAX_SPEED, alt_vel=MAX_SPEED):
-            """Starts both motors (azimuth and altitude) with the given velocities."""
-            az_direction = 1 if az_vel > 0 else 0
-            alt_direction = 1 if alt_vel > 0 else 0
-            motors.setSpeeds(az_vel, alt_vel)
-            self.velocities["az"] = (az_direction, abs(az_vel))
-            self.velocities["alt"] = (alt_direction, abs(alt_vel))
-
-        def reverse(self, motor):
-            """Reverses the specified motor (azimuth or altitude)."""
-            if self.should_reverse(motor):
-                if motor in self.velocities:
-                    current_direction, current_speed = self.velocities[motor]
-                    new_direction = 0 if current_direction == 1 else 1
-                    new_speed = -current_speed if current_direction == 1 else current_speed
-                    if motor == "az":
-                        motors.motor1.setSpeed(new_speed)  # motor1 for azimuth
-                    elif motor == "alt":
-                        motors.motor2.setSpeed(new_speed)  # motor2 for altitude
-                    self.velocities[motor] = (new_direction, abs(new_speed))
-
-        def stop(self, motors=["az", "alt"]):
-            """Stops specified motors (azimuth and/or altitude)."""
-            for motor in motors:
+    def reverse(self, motor):
+        """Reverses the specified motor (azimuth or altitude)."""
+        if self.should_reverse(motor):
+            if motor in self.velocities:
+                current_direction, current_speed = self.velocities[motor]
+                new_direction = 0 if current_direction == 1 else 1
+                new_speed = -current_speed if current_direction == 1 else current_speed
                 if motor == "az":
-                    motors.motor1.setSpeed(0)
+                    motors.motor1.setSpeed(new_speed)  # motor1 for azimuth
                 elif motor == "alt":
-                    motors.motor2.setSpeed(0)
-                self.velocities[motor] = (self.velocities[motor][0], 0)
+                    motors.motor2.setSpeed(new_speed)  # motor2 for altitude
+                self.velocities[motor] = (new_direction, abs(new_speed))
 
-        def stow(self):
-            """Stow method for returning motors to a home position."""
-            raise NotImplementedError("Stow method not implemented.")
+    def stop(self, motors=["az", "alt"]):
+        """Stops specified motors (azimuth and/or altitude)."""
+        for motor in motors:
+            if motor == "az":
+               motors.motor1.setSpeed(0)
+            elif motor == "alt":
+                motors.motor2.setSpeed(0)
+            self.velocities[motor] = (self.velocities[motor][0], 0)
+
+    def stow(self):
+        """Stow method for returning motors to a home position."""
+        raise NotImplementedError("Stow method not implemented.")
     
-except ImportError:
-    pass
