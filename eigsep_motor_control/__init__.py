dd0e168c823bbff61ea4c9ab7eb9ea4fed48421f
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-04-30 18:03:09 -0700

    removed try/except from init.py to see if its necessary or not

--- eigsep_motor_control/__init__.py
+++ eigsep_motor_control/__init__.py
@@ -2,10 +2,10 @@ __author__ = "EIGSEP Team"
 __version__ = "0.0.1"
 
 from .encoder import Encoder, Potentiometer
-from .motor import Motor, QwiicMotor
-try: 
-    from .motor import PoluluMotor
-except:
-    pass
+from .motor import Motor, QwiicMotor, PoluluMotor
+#try: 
+#    from .motor import PoluluMotor
+#except:
+#    pass
 from .limit_switch_hit import reverse_limit
 
