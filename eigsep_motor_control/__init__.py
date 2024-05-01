6beb2c2177fa1eca2dff3da0c5e6b46550f9e9cf
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-04-30 17:42:06 -0700

    ignore import of PoluluMotor for git

--- eigsep_motor_control/__init__.py
+++ eigsep_motor_control/__init__.py
@@ -2,6 +2,10 @@ __author__ = "EIGSEP Team"
 __version__ = "0.0.1"
 
 from .encoder import Encoder, Potentiometer
-from .motor import Motor, QwiicMotor, PoluluMotor
+from .motor import Motor, QwiicMotor
+try: 
+    from .motor import PoluluMotor
+except ImportError:
+    pass
 from .limit_switch_hit import reverse_limit
 
