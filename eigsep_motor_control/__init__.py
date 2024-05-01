8828005fb1e024f1fc5de143d4929e77886a21a9
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-04-30 18:04:48 -0700

    removed try/except commented out code

--- eigsep_motor_control/__init__.py
+++ eigsep_motor_control/__init__.py
@@ -3,9 +3,5 @@ __version__ = "0.0.1"
 
 from .encoder import Encoder, Potentiometer
 from .motor import Motor, QwiicMotor, PoluluMotor
-#try: 
-#    from .motor import PoluluMotor
-#except:
-#    pass
 from .limit_switch_hit import reverse_limit
 
