919d23c5da5342b2cc73900f9b0311417ef2953c
Christian Hellum Bye <chbye@berkeley.edu>
2024-05-15 15:58:29 -0700

    change encoder.py to potentiometer.py

--- eigsep_motor_control/__init__.py
+++ eigsep_motor_control/__init__.py
@@ -1,6 +1,9 @@
 __author__ = "EIGSEP Team"
 __version__ = "0.0.1"
 
-from .encoder import Potentiometer
-from .motor import QwiicMotor, PololuMotor
 from .limit_switch_hit import reverse_limit
+try:
+    from .motor import PololuMotor, QwiicMotor
+except ImportError:
+    pass
+from .potentiometer import Potentiometer
