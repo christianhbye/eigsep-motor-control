6df604639b33a4ae3107ad937cb324b26994570f
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-05-31 01:42:21 -0700

    import dummy motor differently from other motor types

--- eigsep_motor_control/__init__.py
+++ eigsep_motor_control/__init__.py
@@ -4,7 +4,8 @@ __version__ = "0.0.1"
 from .limit_switch_hit import reverse_limit
 
 try:
-    from .motor import PololuMotor, QwiicMotor, DummyMotor
+    from .motor import PololuMotor, QwiicMotor
 except ImportError:
     pass
+from .motor import DummyMotor
 from .potentiometer import Potentiometer, DummyPotentiometer
