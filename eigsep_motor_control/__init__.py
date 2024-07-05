35ea8dee48c875833bc092baea8effe3390b7640
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-07-05 11:48:53 -0700

    PIDController added to init file

--- eigsep_motor_control/__init__.py
+++ eigsep_motor_control/__init__.py
@@ -2,6 +2,7 @@ __author__ = "EIGSEP Team"
 __version__ = "0.0.1"
 
 from .limit_switch_hit import reverse_limit
+from .pid_controller import PIDController
 
 try:
     from .motor import PololuMotor, QwiicMotor, DummyMotor
