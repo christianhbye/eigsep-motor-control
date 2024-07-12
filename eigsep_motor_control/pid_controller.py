87a0cbc3d0651ccb7c630af70a72ad361d084714
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-07-12 14:23:32 -0700

    changes

--- eigsep_motor_control/pid_controller.py
+++ eigsep_motor_control/pid_controller.py
@@ -1,22 +1,22 @@
 import time
 
 class PIDController:
-    def __init__(self, kp, ki, kd, setpoint):
+    def __init__(self, kp, ki, kd):
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.setpoint = setpoint
         self.last_error = 0
         self.integral = 0
         self.last_time = time.time()
 
-    def update(self, measurement):
+    def update(self, setpoint, measurement):
         current_time = time.time()
         delta_time = current_time - self.last_time
-        error = self.setpoint - measurement
+        error = setpoint - measurement
         self.integral += error * delta_time
         derivative = (error - self.last_error) / delta_time if delta_time > 0 else 0
         output = self.kp * error + self.ki * self.integral + self.kd * derivative
         self.last_error = error
         self.last_time = current_time
         return output
+
